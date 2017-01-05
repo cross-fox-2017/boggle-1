0a4fd1a6fca09b4e86fd9b3e2fa390ec6f705780
Syanmil <syanmil@yahoo.com>
2017-01-05 14:08:30 +0700

    simple board shake with bonus finished

--- boggle.js
+++ boggle.js
@@ -8,10 +8,13 @@ class BoogleBoard{
     for (let i = 0; i < this.panjang; i++) {
       this.board[i] = []
       for (let j = 0; j < this.lebar; j++){
-        this.board[i].push("A")
+        this.board[i].push(this.randomer())
       }
     }
     return this.board
+  }
+  randomer(){
+    return String.fromCharCode(Math.random()*(90-65)+65)
   }
 }
 
